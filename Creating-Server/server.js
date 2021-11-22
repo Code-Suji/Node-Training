358cfc65780dc5d4811b3c2d40320d34deca99a3
Sujith C S <sujith.cs@gadgeon.com>
2021-11-22 19:15:03 +0530

    Day 1 work

---
+++ Creating-Server/server.js
@@ -0,0 +1,55 @@
+const http = require(`http`);
+const fs = require(`fs`)
+
+const hostname = '127.0.0.1';
+
+const port = '8000';
+
+const server = http.createServer((req,res) => {
+    // const json = {
+    //     time:Date.now.(),
+    //     name:"Sujith C S",
+    //     email:"sujith.cs@gadgeon.com"
+    // }
+    if(req.url == '/home'){
+        fs.readFile('./view/home.html',null,(error,content) => {
+            if(error){
+                
+                res.writeHead(404)
+                console.log(error)
+                res.write(`Error occured !${JSON.stringify(error)}`)
+                res.end()
+            }
+            else{
+                res.writeHead(200,{'content-type' : 'text/html'})
+                res.write(content)
+                res.end()
+
+            }
+        })
+        
+    }else if (req.url == '/test'){
+        fs.readFile('./view/test.html',null,(error,content) => {
+            if(error){
+                
+                res.writeHead(404)
+                console.log(error)
+                res.write(`Error occured ! ${JSON.stringify(error)}`)
+                res.end()
+            }
+            else{
+                res.writeHead(200,{'content-type' : 'text/html'})
+                res.write(content)
+                res.end()
+
+            }
+        })
+
+    }else {
+        res.end('Hello World')
+    }
+})
+
+server.listen(port , hostname,( ) => {
+    console.log(`server is running in ${port}`);
+})
